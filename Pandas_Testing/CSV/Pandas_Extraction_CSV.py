4dc6e31f914a36e2d5578f3d468ca90556a55958
Jeff Vargas Barrantes <91436004+JeffVar00@users.noreply.github.com>
2023-02-23 13:02:24 -0600

    Update Pandas_Extraction_CSV.py

--- Pandas_Testing/CSV/Pandas_Extraction_CSV.py
+++ Pandas_Testing/CSV/Pandas_Extraction_CSV.py
@@ -1,36 +1,47 @@
 from pyspark.sql import SparkSession
-import pandas as pd
+from pyspark.sql.functions import pandas_udf, PandasUDFType
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, TimestampType
+# import pandas as pd
+from datetime import datetime
 
 # SparkSession
 spark = SparkSession.builder.appName("CSV/Pandas to HDFS").master("spark://localhost:7077").getOrCreate()
 
-# read JSON file into a DataFrame using pandas
-df = pd.read_csv("Friends.csv")
+# pandas transformation from the pyspark trasnformation I did in the previous snippet
+def transform_df(df):
 
-# show the dataframe, the first 10 values
-print(df.head(20))
+    # replace null values in specific columns with a default value
+    default_values = {'name' : "Unknown"}
+    df.fillna(value=default_values, inplace=True)
 
-# show if there is any missing values
-print(df.isnull().sum())
+    # interpolate the age column
+    df["age"] = df["age"].interpolate(method="linear")
 
-# replace null values in specific columns with a default value
-default_values = {'name' : "Unknown"}
-df.fillna(value=default_values, inplace=True)
+    # fill the rest of the columns with a N/A value
+    df.fillna(value="N/A", inplace=True)
 
-# interpolate the age column
-df["age"] = df["age"].interpolate(method="linear")
+    df_pandas = df[['userID', 'name', 'age', 'friends']]
+    df_pandas = df_pandas[df_pandas['age'] < 30]
+    df_pandas['insert_ts'] = datetime.now()
+    df_pandas = df_pandas.sort_values(by=['userID'])
+    df_pandas = df_pandas.reset_index(drop=True)
 
-# fill the rest of the columns with a N/A value
-df.fillna(value="N/A", inplace=True)
+    return df_pandas
 
-# show if there is any missing values
-print(df.isnull().sum())
+df = spark.read.csv("Friends.csv", header=True, inferSchema=True)
+
+schema = StructType([StructField("userID", IntegerType()), StructField("name", StringType()), StructField("age", IntegerType()), StructField("friends", IntegerType()), StructField("insert_ts", TimestampType())])
 
 # transform into a spark dataframe to upload it to HDFS
-df_spark = spark.createDataFrame(df)
+@pandas_udf(schema, functionType=PandasUDFType.GROUPED_MAP)
+def transform_df_to_udf(pdf):
+    # apply the transformation
+    return transform_df(pdf)
+
+df = df.groupby("userID").apply(transform_df_to_udf)
 
 # write the DataFrame to HDFS
-df_spark.write\
+df.write\
 .format("parquet").mode("overwrite")\
 .option("path", "hdfs://localhost:9000/user/hadoop_ADMIN/spark/CSV_to_parquet/")\
 .partitionBy("age")\
