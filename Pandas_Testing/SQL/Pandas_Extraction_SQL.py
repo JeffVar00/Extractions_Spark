e2fa8914b5506ef9980981aeb6f04e5f3f58588a
Jeff Vargas Barrantes <91436004+JeffVar00@users.noreply.github.com>
2023-02-24 10:47:30 -0600

    better sql extraction
    same times? interesting

--- Pandas_Testing/SQL/Pandas_Extraction_SQL.py
+++ Pandas_Testing/SQL/Pandas_Extraction_SQL.py
@@ -6,29 +6,6 @@ from pyspark.sql.functions import pandas_udf, PandasUDFType
 
 spark = SparkSession.builder.appName("SQL/Pandas to HDFS").master("spark://localhost:7077").getOrCreate()
 
-def transformation_df(df):
-    # show the dataframe, the first 10 values
-    print(df.head(10))
-
-    # show if there is any missing values
-    print(df.isnull().sum())
-
-    # pandas version
-    df.dropna(how='any', inplace=True)
-
-    # axis ->
-    #   0 or ‘index’ : Drop rows which contain missing values.
-    #   1 or ‘columns’ : Drop columns which contain missing value.
-    #
-    # how ->
-    #   ‘any’ : If any NA values are present, drop that row or column.
-    #   ‘all’ : If all values are NA, drop that row or column.
-
-    print(df.isnull().sum())
-
-    return df
-
-
 jdbcURL = "jdbc:sqlserver://172.19.128.1:1433;databaseName=AdventureWorks2019"
 table = 'Person.Address'
 user = 'jeff'
@@ -41,15 +18,14 @@ df = spark.read.format("jdbc")\
     .option('password', pwd)\
     .load()
 
-
 @pandas_udf(df.schema, functionType=PandasUDFType.GROUPED_MAP)
-def transform_df_to_udf(pdf):
-    return transformation_df(pdf)
+def drop_nulls(df):
+    df.dropna(how='any', inplace=True)
+    return df
 
-df_spark = df.apply(transform_df_to_udf)
+df = df.groupby().apply(drop_nulls)
 
-
-df_spark.write\
+df.write\
 .format("parquet").mode("overwrite")\
 .option("path", "hdfs://localhost:9000/user/hadoop_ADMIN/spark/SQL_to_parquet/")\
 .partitionBy("City")\
